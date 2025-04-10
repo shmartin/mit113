4fc6509b7d272f23cf0bb34da2be1077b6b8fcd8
shmartin <abadjuanmartin@gmail.com>
2025-04-10 22:02:06 +0800

    updated UI

--- finals/inventory/views.py
+++ finals/inventory/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def inventory_render(request):
-    return render(request, 'inventory/inventory_render.html')
+    return render(request, 'inventory/inventory.html')
