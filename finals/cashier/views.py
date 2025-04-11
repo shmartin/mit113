965c40a6ada2c0cecd8b7f17860c5bf21e95e48c
shmartin <abadjuanmartin@gmail.com>
2025-04-11 11:29:00 +0800

    added dynamic UI to inventory

--- finals/cashier/views.py
+++ finals/cashier/views.py
@@ -2,4 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def cashier(request):
-    return render(request, 'cashier/cashier.html')
+    meals = [   'Beef Tapa',
+                'Beef Pares',
+                'Beef Nilaga',
+                'Beef Walastik',
+                'Boneless Bangus',
+                'Garlic Longganisa',
+                'Spicy Longganisa',
+                'Delicious Longganisa',
+                'Grilled Porkchop',
+                'Grilled Liempo',
+                'Pork Adobo',
+                'Pork Marinated',
+                'Pork Tocino',
+                'Pork Steak',
+                'Pork Sisig',
+                'Lechon Kawali',
+                'Crispy Kare-kare',
+                'Crispy Binagoongan',
+                'Pigar-Pigar',
+                'Salpicao',
+                'Chicken Pastil',
+            ]
+    return render(request, 'cashier/cashier.html', {'meals': meals})
