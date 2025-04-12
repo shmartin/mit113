91452e38ee1b9e6b9b7ba277e5bafbe4c07e0f1e
shmartin <abadjuanmartin@gmail.com>
2025-04-12 11:22:47 +0800

    general cleanup

--- finals/inventory/views.py
+++ finals/inventory/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+import random as rand
 
 # Create your views here.
 def inventory_render(request):
-    return render(request, 'inventory/inventory.html')
+    meals = [   'Beef Tapa',
+                'Beef Pares',
+                'Beef Nilaga',
+                'Beef Walastik',
+                'Boneless Bangus',
+                'Garlic Longganisa',
+                'Spicy Longganisa',
+                'Delicious Longganisa',
+                'Grilled Porkchop',
+                'Grilled Liempo',
+                'Pork Adobo',
+                'Pork Marinated',
+                'Pork Tocino',
+                'Pork Steak',
+                'Pork Sisig',
+                'Lechon Kawali',
+                'Crispy Kare-kare',
+                'Crispy Binagoongan',
+                'Pigar-Pigar',
+                'Salpicao',
+                'Chicken Pastil',
+            ]
+    quantity = rand.sample(range(0,30), len(meals))
+    return render(request, 'inventory/inventory.html', {'meals': meals, 'quantity': quantity})
